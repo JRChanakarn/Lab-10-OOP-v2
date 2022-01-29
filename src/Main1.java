4826baf3f3015946b13db1a356cea7c63f4242fa
chanakarnprasansuk <chanakarnprasansuk@localhost>
2022-01-29 17:52:09 +0700

    Test dynamic clock Now it Work in console  But it not Working in GUI

--- src/Main1.java
+++ src/Main1.java
@@ -10,14 +10,28 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.wb.swt.SWTResourceManager;
 
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter; 
-
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 
 public class Main1 {
 
 	protected Shell shell;
-	private Text text;
+	
+	private String time;
+	SimpleDateFormat timeFormat;	
+
+	private String JTex;
+	private Label lblCl;
+	private Label lblcl2;
+	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+	
 	
 	
 	/**
@@ -25,9 +39,13 @@ public class Main1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+
 		try {
+			
 			Main1 window = new Main1();
 			window.open();
+		 	
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -35,6 +53,61 @@ public class Main1 {
 		
 	}
 
+	
+	public void setTime() {
+		
+		Thread clock = new Thread() {
+
+			
+
+			public void run() {
+				
+				try {
+					
+					for(;;) {
+						
+						Calendar cal = new GregorianCalendar();
+						int  sec = cal.get(Calendar.SECOND);
+						int  minute = cal.get(Calendar.MINUTE);
+						int  houre = cal.get(Calendar.HOUR);
+						
+						
+						System.out.println("Time  : "+houre+":"+ minute +":"+ sec);
+						//lblNewLabel.setText("Time"+houre + minute + sec);
+					
+						
+						JTex = houre+":"+ minute +":"+ sec;
+						System.out.println("JTex  : "+JTex);
+						
+						//jtext.setText("Time"+houre + minute + sec);
+						 
+						 
+					sleep(1000);
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+			}
+		
+		
+		
+	};
+	
+	clock.start();
+}
+		
+	
+	
+	public Main1() {
+		
+		setTime();
+
+
+		
+	}
 	
 	/**
 	 * Open the window.
@@ -45,16 +118,24 @@ public class Main1 {
 		createContents();
 		shell.open();
 		shell.layout();
-		
+	
 		while (!shell.isDisposed()) {
-
+			
 			if (!display.readAndDispatch()) {
+				
 				display.sleep();
+				
 			}
 		}
 		
 	}
 
+	
+	
+	
+	
+	
+	
 	/**
 	 * Create contents of the window.
 	 */
@@ -93,24 +174,35 @@ public class Main1 {
 		lblM.setText("Minute");
 		
 		Label lblS = new Label(composite, SWT.NONE);
+		lblS.setFont(SWTResourceManager.getFont(".AppleSystemUIFont", 11, SWT.NORMAL));
 		lblS.setBounds(250, 10, 59, 14);
 		lblS.setText("Second");
 		
+
+		
+
+		
+	    lblCl = new Label(composite, SWT.NONE);
+		lblCl.setBounds(35, 30, 289, 42);
+		lblCl.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
+		
+		lblcl2 = formToolkit.createLabel(composite, "Clock", SWT.NONE);
+		lblcl2.setBounds(35, 89, 152, 37);
+		
+		
+		
 		
 		DateTime dateTime = new DateTime(composite, SWT.BORDER | SWT.TIME);
-		dateTime.setFont(SWTResourceManager.getFont(".AppleSystemUIFont", 66, SWT.NORMAL));
-		dateTime.setBounds(38, 30, 336, 96);
-		
-		text = new Text(shell, SWT.BORDER);
-		text.setBounds(87, 206, 320, 33);
-		text.setText(FT);
+		dateTime.setBounds(216, 86, 105, 28);
+		formToolkit.adapt(dateTime);
+		formToolkit.paintBordersFor(dateTime);
+		lblcl2.setText(JTex);
 		
 		Button btnNewButton = new Button(shell, SWT.NONE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			
 			public void widgetSelected(SelectionEvent e) {
-				String Tm = st.setTime();
-			
+				
 			
 				
 				
@@ -145,11 +237,9 @@ public class Main1 {
 		});
 		btnNewButton_3.setBounds(247, 231, 177, 49);
 		btnNewButton_3.setText("Stop");
-
-//		Settime st = new Settime();
-//		st.setTime();
+		
+		
+		setTime();
 		
 	}
-	
-	
 }
--- src/Settime.java
+++ src/Settime.java
@@ -1,5 +1,6 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Settime {
 
@@ -28,3 +29,97 @@ public class Settime {
 	
 	
 }
+
+
+
+//public void setTime() {
+//	timeFormat = new SimpleDateFormat("hh:mm:ss a");
+//	
+//	
+//	Thread clock = new Thread() {
+//		
+//		public void run() {
+//			
+//			  while(true) {
+//				  
+//		
+//				  try {
+//				   time = timeFormat.format(Calendar.getInstance().getTime());
+//				   text.setText(time);
+//				   System.out.println(time);
+//				   sleep(1000);
+//				  } catch (InterruptedException e) {
+//				 
+//				   e.printStackTrace();
+//				  }
+//				  }
+//			
+//		}
+//	
+//	 };
+//	 clock.start();
+//}
+
+
+
+//public void setTime() {
+//	
+//	Thread clock = new Thread() {
+//
+//		
+//
+//			public void run() {
+//				
+//				try {
+//					
+//					for(;;) {
+//						
+//						Calendar cal = new GregorianCalendar();
+//						 sec = cal.get(Calendar.SECOND);
+//						 minute = cal.get(Calendar.MINUTE);
+//						 houre = cal.get(Calendar.HOUR);
+//						
+//						
+//						//System.out.println("Time  : "+houre+":"+ minute +":"+ sec);
+//						//lblNewLabel.setText("Time"+houre + minute + sec);
+//						
+//					
+//						 text.setText("Time"+houre + minute + sec);
+//					sleep(1000);
+//					}
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//				
+//			}
+//		
+//		
+//		
+//	};
+//	
+//	clock.start();
+//	
+//}
+
+
+
+//
+//
+//Calendar cal = new GregorianCalendar();
+// sec = cal.get(Calendar.SECOND);
+// minute = cal.get(Calendar.MINUTE);
+// houre = cal.get(Calendar.HOUR);
+//
+// String s=String.valueOf(sec);
+//
+//
+//System.out.println("Time  : "+houre+":"+ minute +":"+ sec);
+//
+////text.setText( "Time  : "+ houre+":"+ minute +":"+ sec );
+////this.Cl.setText("Time  : ");
+
+	
+	
+	
